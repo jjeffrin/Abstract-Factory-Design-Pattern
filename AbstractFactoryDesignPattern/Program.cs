58fa82317f77906ecbb022926fd4f3e5c5a08a18
jjeffrin <jjeffrin@gmail.com>
2019-07-30 14:20:33 +0530

    Final Commit

--- AbstractFactoryDesignPattern/Program.cs
+++ AbstractFactoryDesignPattern/Program.cs
@@ -2,11 +2,141 @@
 
 namespace AbstractFactoryDesignPattern
 {
+    public interface RideFactory
+    {
+        Bike GetBike(string Bike);
+        Scooter GetScooter(string Scooter);
+    }
+
+    public class HondaFactory : RideFactory
+    {
+        public Bike GetBike(string Bike)
+        {
+            switch (Bike.ToLower())
+            {
+                case "sport":
+                    return new SportBike();
+                case "regular":
+                    return new RegularBike();
+                default:
+                    throw new ApplicationException("Error");
+            }
+        }
+
+        public Scooter GetScooter(string Scooter)
+        {
+            switch (Scooter.ToLower())
+            {
+                case "sport":
+                    return new Scooty();
+                case "regular":
+                    return new RegularScooter();
+                default:
+                    throw new ApplicationException("Error");
+            }
+        }
+    }
+
+    public class SuzukiFactory : RideFactory
+    {
+        public Bike GetBike(string Bike)
+        {
+            switch (Bike.ToLower())
+            {
+                case "sport":
+                    return new SportBike();
+                case "regular":
+                    return new RegularBike();
+                default:
+                    throw new ApplicationException("Error");
+            }
+        }
+
+        public Scooter GetScooter(string Scooter)
+        {
+            switch (Scooter.ToLower())
+            {
+                case "sport":
+                    return new Scooty();
+                case "regular":
+                    return new RegularScooter();
+                default:
+                    throw new ApplicationException("Error");
+            }
+        }
+    }
+
+    public interface Scooter
+    {
+        string Name();
+    }
+
+    public class Scooty: Scooter
+    {
+        public string Name()
+        {
+            return "Scooter - Sports";
+        }
+    }
+
+    public class RegularScooter: Scooter
+    {
+        public string Name()
+        {
+            return "Scooter - Regular";
+        }
+    }
+
+    public interface Bike
+    {
+        string Name();
+    }
+
+    public class SportBike: Bike
+    {
+        public string Name()
+        {
+            return "Bike - Sport";
+        }
+    }
+
+    public class RegularBike: Bike
+    {
+        public string Name()
+        {
+            return "Bike - Regular";
+        }
+    }
+
+    public class RideClient
+    {
+        Scooter scooter;
+        Bike bike;
+
+        public RideClient(RideFactory factory, string model)
+        {
+            scooter = factory.GetScooter(model);
+            bike = factory.GetBike(model);
+        }
+
+        public string GetScooterName()
+        {
+            return scooter.Name();
+        }
+
+        public string GetBikeName()
+        {
+            return bike.Name();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Abstract Factory Design Pattern");
+            RideClient client = new RideClient(new HondaFactory(), "Sport");
+            Console.WriteLine(client.GetBikeName());
         }
     }
 }
